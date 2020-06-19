af95b56696b667e186597d3733bcd5439b922e60
NasratTalash <ntalash28@gmail.com>
2020-06-20 01:35:02 +0530

    Roll button functionality is coded

--- app.js
+++ app.js
@@ -10,78 +10,89 @@ GAME RULES:
 */
 
 let dom = {
-  rollBtn: document.querySelector('.btn-roll'),
-  newBtn: document.querySelector('.btn-new'),
-  holdBtn: document.querySelector('.btn-hold'),
-  dice: document.querySelector('.dice'),
-  playerOne: {
-    roundScore: document.querySelector('#current-0'),
-    globalScore: document.querySelector('#score-0'),
-    panel: document.querySelector('.player-0-panel'),
-  },
-  playerTwo: {
-    roundScore: document.querySelector('#current-1'),
-    globalScore: document.querySelector('#score-1'),
-    panel: document.querySelector('.player-1-panel'),
-  },
+  rollBtn: document.querySelector(".btn-roll"),
+  newBtn: document.querySelector(".btn-new"),
+  holdBtn: document.querySelector(".btn-hold"),
+  dice: document.querySelector(".dice"),
+  player: [
+    {
+      roundScore: document.querySelector("#current-0"),
+      globalScore: document.querySelector("#score-0"),
+      panel: document.querySelector(".player-0-panel"),
+    },
+    {
+      roundScore: document.querySelector("#current-1"),
+      globalScore: document.querySelector("#score-1"),
+      panel: document.querySelector(".player-1-panel"),
+    },
+  ],
 };
-let scores = {
-  playerOne: {
+let playerScore = [
+  {
     roundScore: 0,
     globalScore: 0,
   },
-  playerTwo: {
+  {
     roundScore: 0,
     globalScore: 0,
   },
-};
+];
+
+let currentPlayer;
 
 // Call the init function
 init();
-// IIFE function to initialize the game 
+// IIFE function to initialize the game
 function init() {
-  dom.playerOne.globalScore.textContent = 0;
-  dom.playerOne.roundScore.textContent = 0;
+  currentPlayer = 0;
+  dom.player.forEach((cur) => {
+    cur.roundScore.textContent = "0";
+    cur.globalScore.textContent = "0";
+  });
 
-  dom.playerTwo.globalScore.textContent = 0;
-  dom.playerTwo.roundScore.textContent = 0;
+  dom.dice.style.display = "none";
 
-  dom.dice.style.display = 'none';
+  dom.player[0].panel.classList.add("active");
+  dom.player[1].panel.classList.remove("active");
+}
 
-  dom.playerOne.panel.classList.add('active');
-  dom.playerTwo.panel.classList.remove('active');
-};
 // To generate the random number
 function generateRandom() {
-  return Math.round((Math.random() * 5) + 1);
+  return Math.round(Math.random() * 5 + 1);
 }
 
 // add round score
 function addRoundScore(score, actPlayer) {
-  if(actPlayer === 0) {
-    scores.playerOne.roundScore += score;
-    if(score === 1) {
-      scores.playerOne.roundScore = 0;
-    }
-  } else if(actPlayer === 1) {
-    scores.playerTwo.roundScore += score;
-    if(score === 1) {
-      scores.playerOne.roundScore = 0;
-    }
+  if (score !== 1) {
+    playerScore[actPlayer].roundScore += score;
+  } else {
+    nextPlayer(actPlayer);
   }
+}
+
+// function to change to next player
+function nextPlayer(actPlayer) {
+  playerScore[actPlayer].roundScore = 0;
+  dom.player[actPlayer].roundScore.textContent = "0";
+  dom.player[actPlayer].panel.classList.remove("active");
+  actPlayer === 0 ? (currentPlayer = 1) : (currentPlayer = 0);
+  dom.player[currentPlayer].panel.classList.add("active");
 }
 
 // rollDice function to roll the dice
 function rollDice() {
   let score = generateRandom();
-  addRoundScore(score, 0);
-  showScore(scores.playerOne.roundScore);
+  addRoundScore(score, currentPlayer);
+  showScore(currentPlayer);
+  console.log(score);
 }
 
 // funtion to show the dice number on ui
-function showScore(number) {
-  dom.playerOne.roundScore.textContent = number;
+function showScore(actPlayer) {
+  dom.player[actPlayer].roundScore.textContent =
+    playerScore[actPlayer].roundScore;
+  console.log(playerScore[actPlayer].roundScore);
 }
 
 // Event Listener for roll button
-dom.rollBtn.addEventListener('click', rollDice);
+dom.rollBtn.addEventListener("click", rollDice);
